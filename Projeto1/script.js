bde7dc63c7d3a8844a1aa013e8a997e417b604a5
Carlos UchOa <carlosjuchoa@hotmail.com>
2021-07-19 16:00:23 -0300

    aplicando o elemento a classe desejada via js

--- Projeto1/script.js
+++ Projeto1/script.js
@@ -10,9 +10,13 @@ document.body.addEventListener('keyup', (event) => {
 function playSound(sound) {
 
     let audioElement = document.querySelector(`#s_${sound}`);
+    let keyElement = document.querySelector(`div[data-key="${sound}"]`);
     //aqui verifica se o audio elemento é o mesmo selecionado que possui som e aplica o som ao elemento
     if (audioElement) {
         audioElement.play();
     }
-
+    //verifica se o elemento existe e atribui a classe active nele, essa classe ja existe no css e deixará com a cor amarela
+    if (keyElement) {
+        keyElement.classList.add('active');
+    }
 }
