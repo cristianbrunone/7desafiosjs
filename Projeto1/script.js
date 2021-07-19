5c228f1a6cbbbebef5fe9760492659d73c5bbce7
Carlos UchOa <carlosjuchoa@hotmail.com>
2021-07-19 16:09:47 -0300

    finalizando a primeira parte do projeto que é mostrar ao clicar nos botões os ons

--- Projeto1/script.js
+++ Projeto1/script.js
@@ -13,10 +13,16 @@ function playSound(sound) {
     let keyElement = document.querySelector(`div[data-key="${sound}"]`);
     //aqui verifica se o audio elemento é o mesmo selecionado que possui som e aplica o som ao elemento
     if (audioElement) {
+        //reseta o tempo do elemento
+        audioElement.currentTime = 0;
         audioElement.play();
     }
     //verifica se o elemento existe e atribui a classe active nele, essa classe ja existe no css e deixará com a cor amarela
     if (keyElement) {
         keyElement.classList.add('active');
+        //aqui ele vai esperar 300 milisegundos e remover a classe senão ela fica permanente
+        setTimeout(() => {
+            keyElement.classList.remove('active');
+        }, 300);
     }
 }
