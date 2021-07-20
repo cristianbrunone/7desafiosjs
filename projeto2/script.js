c2794d1e6ef5414e3267d1b4ebfd655bccb421eb
Carlos UchOa <carlosjuchoa@hotmail.com>
2021-07-20 09:32:33 -0300

    aplicando o zero na frente do valor menor que 10 na hora e corrigindo assim

--- projeto2/script.js
+++ projeto2/script.js
@@ -10,7 +10,30 @@ let hElement = document.querySelector('.p_h');
 
 //vamos criar um time que de 1 em 1 seg ele posiciona os ponteiros confome for necessario
 function updateClock() {
+    //aqui chamo a função Date que é responsavel pela data hora minutos, segundos, dias, ano, mês, etc
+    let now = new Date();
+    //aqui chamo as horas
+    let hour = now.getHours();
+    //aqui chamo os minutos
+    let minute = now.getMinutes();
+    //aqui chamo os segundos
+    let second = now.getSeconds();
 
+    //inserindo a hora no relógio digital
+
+    digitalElement.innerHTML = `${fixZero(hour)}:${fixZero(minute)}:${fixZero(second)}`;
+
+}
+
+//adicionando um zero antes do valor menor do que 10 para corrigir a hora.
+//se o time for menor que 10, ele retornara um zero junto com o time, se não retornará apenas o time
+
+function fixZero(time) {
+    if (time < 10) {
+        return '0' + time;
+    } else {
+        return time;
+    }
 }
 
 //aqui ele vai executar de 1 em 1 segundo.
