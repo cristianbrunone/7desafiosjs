040fd6bf194cd2263eb6013116452fe8b038e5f4
Carlos UchOa <carlosjuchoa@hotmail.com>
2021-07-21 13:05:40 -0300

    verificando oque o user digitou no input

--- projeto3/script.js
+++ projeto3/script.js
@@ -1,6 +1,12 @@
 //vamos começar pegando as informações do form
 //e estamos prevenindo que o usuário digite uma letra errada ou faça alguma ação indesejada com o preventDefault
+
 document.querySelector('.busca').addEventListener('submit', (event) => {
     event.preventDefault();
+
+    let input = document.querySelector('#searchInput').value;
+    //agora ja tenho acesso ao que o usuario digitou 
+
+    console.log(input);
 
 });
