f454b69be26e328ea4948c6ff3e9df0e4f5ad7ce
carlos <uchoa@freeline.inf.br>
2021-09-02 15:36:49 -0300

    aplicando elementos de manipulaçao no DOM

--- projeto6/script.js
+++ projeto6/script.js
@@ -9,7 +9,19 @@ function showQuestion() { //
     if(questions[currentQuestion]) {
         let q = questions[currentQuestion]; 
 
-        console.log(q.question);
+        //console.log(q.question);
+        document.querySelector('.scoreArea').style.display = 'none';
+        document.querySelector('.questionArea').style.display = 'block';
+
+        document.querySelector('.question').innerHTML = q.question;
+
+
+        let optionsHtml = '';
+        for(let i in q.options) {
+            optionsHtml += `<div class="option"><span>${parseInt(i)+1}</span>${q.options[i]} </div>`;
+        }
+
+       document.querySelector('.options').innerHTML = optionsHtml;
     }else {
         //acabaram as questões
     }
