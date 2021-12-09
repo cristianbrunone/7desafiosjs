8eb879787880308f096e5854f5690464ee661416
Carlos Uchoa <carlosjuchoa@hotmail.com>
2021-12-09 16:19:28 -0300

    criando logica de click

--- projeto6/script.js
+++ projeto6/script.js
@@ -18,10 +18,13 @@ function showQuestion() { //
 
         let optionsHtml = '';
         for(let i in q.options) {
-            optionsHtml += `<div class="option"><span>${parseInt(i)+1}</span>${q.options[i]} </div>`;
+            optionsHtml += `<div data-op="${i}" class="option"><span>${parseInt(i)+1}</span>${q.options[i]} </div>`;
         }
 
        document.querySelector('.options').innerHTML = optionsHtml;
+       document.querySelectorAll('.options .option').forEach(item => {
+            item.addEventListener('click', optionClickEvent);
+       });
     }else {
         //acabaram as questões
     }
