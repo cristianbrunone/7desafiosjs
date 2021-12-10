2190e661159211b4beda592cd6a33370ed4bfb46
Carlos Uchoa <carlosjuchoa@hotmail.com>
2021-12-10 16:46:16 -0300

    criando area clicavel e arrastavel

--- projeto7/script.js
+++ projeto7/script.js
@@ -17,14 +17,23 @@ document.querySelectorAll('.item').forEach(item =>{
     item.addEventListener('dragend', dragEnd);
 });
 
+//toda area que vc quiser criar, precisa ter estes 3 eventos , meio que obrigatório, existem outros, mas estes 3 são os principais
+document.querySelectorAll('.area').forEach(area => {
+    area.addEventListener('dragover', dragOver);
+    area.addEventListener('dragleave', dragLeave);
+    area.addEventListener('drop', drop);
+});
+
 
 //Funções dos Itens
 function dragStart(event) {
-
+//quando começar a arrastar o item vou colocar uma classe 
+event.currentTarget.classList.add('dragging');
 }
 
 function dragEnd(event) {
-
+    //e quando ele parar vou remover essa class
+    event.currentTarget.classList.remove('dragging');
 }
 
 //Funções da Area
