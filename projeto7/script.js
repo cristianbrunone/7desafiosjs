6620b48b314220e05f9c414dfc6f8d4ade0dc459
Carlos Uchoa <carlosjuchoa@hotmail.com>
2021-12-10 16:36:13 -0300

    criando as funções de eventos

--- projeto7/script.js
+++ projeto7/script.js
@@ -1,4 +1,4 @@
-document.querySelector('.neutralArea').addEventListener('click', (event) => {
+// document.querySelector('.neutralArea').addEventListener('click', (event) => {
     // console.log("clicou!");
     //para saber onde clicou em qual area mais especifico
     // console.log("Target", event.target);
@@ -6,6 +6,26 @@ document.querySelector('.neutralArea').addEventListener('click', (event) => {
     // para verificar onde realmente eu estou clicando existem duas maneiras, vamos ver detalhes
     // console.log("TARGET", event.target);
     // console.log("CURRENT TARGET", event.currentTarget);
+// });
 
-    
+
+
+//vamos selecionar todos os items
+
+document.querySelectorAll('.item').forEach(item =>{
+    item.addEventListener('dragstart', dragStart);
+    item.addEventListener('dragend', dragEnd);
 });
+
+
+//Funções dos Itens
+function dragStart(event) {
+
+}
+
+function dragEnd(event) {
+
+}
+
+//Funções da Area
+
