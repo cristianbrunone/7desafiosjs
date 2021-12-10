fc2ebc122463b8fbce52d00c43c109bd22af4472
Carlos Uchoa <carlosjuchoa@hotmail.com>
2021-12-10 16:30:37 -0300

    aplicando logica para arrastar item

--- projeto7/script.js
+++ projeto7/script.js
@@ -2,5 +2,10 @@ document.querySelector('.neutralArea').addEventListener('click', (event) => {
     // console.log("clicou!");
     //para saber onde clicou em qual area mais especifico
     // console.log("Target", event.target);
-    event.target.style.border ='1px solid #FF0000';
+    // event.target.style.border ='1px solid #FF0000';
+    // para verificar onde realmente eu estou clicando existem duas maneiras, vamos ver detalhes
+    // console.log("TARGET", event.target);
+    // console.log("CURRENT TARGET", event.currentTarget);
+
+    
 });
