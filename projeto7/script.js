2ea968227e9a53667186c08a96b51f9ee6be7c74
Carlos Uchoa <carlosjuchoa@hotmail.com>
2021-12-13 11:06:13 -0300

    criando cores e condições especificas para zonas de drops diferentes

--- projeto7/script.js
+++ projeto7/script.js
@@ -8,8 +8,12 @@
     // console.log("CURRENT TARGET", event.currentTarget);
 // });
 
-
-
+//aqui irei implementar uma logica para deixar visual as areas quando estiverem ocupadas pelos itens, por enquanto apenas estou informando que estão sem nada, nulas
+let areas = {
+    a: null,
+    b: null,
+    c: null
+};
 //vamos selecionar todos os items
 
 document.querySelectorAll('.item').forEach(item =>{
@@ -31,8 +35,8 @@ document.querySelector('.neutralArea').addEventListener('drop', dropNeutral);
 
 //Funções dos Itens
 function dragStart(event) {
-//quando começar a arrastar o item vou colocar uma classe 
-event.currentTarget.classList.add('dragging');
+    //quando começar a arrastar o item vou colocar uma classe 
+    event.currentTarget.classList.add('dragging');
 }
 
 function dragEnd(event) {
@@ -45,9 +49,9 @@ function dragEnd(event) {
 function dragOver(event) {
     //dessa forma eu só adiciona estes elementos quando nao tiver nenhuma area selecionada, ai eles irão aparecer
 if(event.currentTarget.querySelector('.item') === null){
-// console.log('PASSOU POR CIMA');
-event.preventDefault();
-event.currentTarget.classList.add('hover');
+    // console.log('PASSOU POR CIMA');
+    event.preventDefault();
+    event.currentTarget.classList.add('hover');
     }    
 
 }
@@ -66,10 +70,11 @@ event.currentTarget.classList.remove('hover');
 // console.log(event.currentTarget);
 //agora para saber se ja tem algum item dentro da area que estou passando por cima 
 //aqui fica a logica de arrastar e soltar dentro de uma area, se essa area ja tiver um item ela nao será aceita para dropar item selecionado
-if(event.currentTarget.querySelector('.item') === null){
+    if(event.currentTarget.querySelector('.item') === null){
     //pegando o item e jogando dentro do quadrado
     let dragItem = document.querySelector('.item.dragging');
     event.currentTarget.appendChild(dragItem);
+    updateAreas();
     }
 }
 
@@ -77,19 +82,42 @@ if(event.currentTarget.querySelector('.item') === null){
 //acima eu capturei as areas neutras e adicionei funções a ela aqui irei criar as funções adicionadas anteriormente
 
 function dragOverNeutral(event){
-event.preventDefault();
-event.currentTarget.classList.add('hover');
+    event.preventDefault();
+    event.currentTarget.classList.add('hover');
 }
 
 function dragLeaveNeutral(event){
-event.currentTarget.classList.remove('hover');
+    event.currentTarget.classList.remove('hover');
 }
 
 function dropNeutral(event){
-event.currentTarget.classList.remove('hover');
-//aqui ele vai pegar o item selecionado 
-let dragItem = document.querySelector('.item.dragging');
-//e aqui eu tiro de onde tiver e coloco na minha area neutra
-event.currentTarget.appendChild(dragItem);
+    event.currentTarget.classList.remove('hover');
+    //aqui ele vai pegar o item selecionado 
+    let dragItem = document.querySelector('.item.dragging');
+    //e aqui eu tiro de onde tiver e coloco na minha area neutra
+    event.currentTarget.appendChild(dragItem);
+    updateAreas();
+
 }
 
+//Funções Lógicas
+
+function updateAreas() {
+    document.querySelectorAll('.area').forEach(area => {
+        let name = area.getAttribute('data-name');
+        if(area.querySelector('.item') !== null){
+            areas[name] = area.querySelector('.item').innerHTML;
+        }else {
+            areas[name] = null;
+        }
+    });
+    // console.log(areas);
+    //vou adicionar uma cor nas areas quando estiverem em sequencia
+    if(areas.a === '1' && areas.b === '2' && areas.c === '3'){
+        //ta na sequencia correta entao
+        document.querySelector('.areas').classList.add('correct');
+    }else {
+        document.querySelector('.areas').classList.remove('correct');
+    }
+
+}
